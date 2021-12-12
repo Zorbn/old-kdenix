d4cb9444ecf043c53e228122678bcff3f484669a
Zorbn <public.zorb@gmail.com>
2021-12-11 20:16:07 -0800

    Added readme

--- configuration.nix
+++ configuration.nix
@@ -47,6 +47,7 @@
     KWIN_X11_REFRESH_RATE = "144000";
     KWIN_X11_NO_SYNC_TO_VBLANK = "1";
     KWIN_X11_FORCE_SOFTWARE_VSYNC = "1";
+    EDITOR = "nvim";
   };
 
   # Enable the Desktop Environment.
